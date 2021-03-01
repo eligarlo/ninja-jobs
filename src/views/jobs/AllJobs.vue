f1565f8fe0f4f23a038bda87779d2a84e2f9e881
Mohit Sehgal <sehgal.mohit29@gmail.com>
2021-03-01 21:45:55 +0530

    style removed

--- src/views/jobs/AllJobs.vue
+++ src/views/jobs/AllJobs.vue
@@ -17,24 +17,3 @@ export default {
   },
 }
 </script>
-
-<style>
-.job h2 {
-  background-color: #f4f4f4;
-  padding: 20px;
-  border-radius: 10px;
-  margin: 10px auto;
-  max-width: 600px;
-  cursor: pointer;
-  color: #444;
-  transition: background-color 0.2s ease-in;
-}
-
-.job h2:hover {
-  background-color: #ddd;
-}
-
-.job a {
-  text-decoration: none;
-}
-</style>
